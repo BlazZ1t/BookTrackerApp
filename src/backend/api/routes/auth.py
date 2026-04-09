bfd2bd5442717adf302e092b54e82eda95cff0af
BlazZ1t <klimspy@gmail.com>
2026-04-09 15:51:23 +0300

    fix: database integration

--- src/backend/api/routes/auth.py
+++ src/backend/api/routes/auth.py
@@ -3,16 +3,15 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import logging
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from src.backend.api.schemas.auth import LoginRegisterRequest, TokenResponse
 
 from passlib.context import CryptContext
 from jose import jwt
 
-from src.backend.api.initialize import service_connections
 import src.backend.database.repository.users as users_repository
-
-database = service_connections.get_connection()
+from src.backend.database.connection import get_database_connection
+import sqlite3
 
 router = APIRouter(prefix="/auth")
 logger = logging.getLogger(__name__)
@@ -28,7 +27,10 @@ pwd_context = CryptContext(
 
 
 @router.post("/register", status_code=201)
-async def register(user: LoginRegisterRequest):
+async def register(
+    user: LoginRegisterRequest,
+    database: sqlite3.Connection = Depends(get_database_connection)
+):
     if users_repository.get_user_by_username(database, user.login):
         raise HTTPException(status_code=409,
                             detail={"message": "Username already taken"}
@@ -36,23 +38,27 @@ async def register(user: LoginRegisterRequest):
     password_hash = hash_password(user.password)
     try:
         users_repository.create_user(database, user.login, password_hash)
+    except HTTPException as e:
+        raise e
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500,
                             detail={"message": "Internal server error"})
     return {"message": "User registered"}
 
-'''
-JWT Tokens are created with user id, to get the user id do:
-payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
-id = payload.get("sub")
-
-Also check expiry prolly
-'''
-
 
 @router.post("/login", response_model=TokenResponse)
-async def login(user: LoginRegisterRequest):
+async def login(
+    user: LoginRegisterRequest,
+    database: sqlite3.Connection = Depends(get_database_connection)
+):
+    """
+    JWT Tokens are created with user id, to get the user id do:
+    payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+    id = payload.get("sub")
+
+    Also check expiry prolly
+    """
     try:
         db_user = users_repository.get_user_by_username(database, user.login)
         if not db_user:
@@ -62,6 +68,8 @@ async def login(user: LoginRegisterRequest):
         if not verify_password(user.password, hashed_password):
             raise HTTPException(status_code=401,
                                 detail={"message": "Invalid credentials"})
+    except HTTPException as e:
+        raise e
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500,
--- src/backend/app.py
+++ src/backend/app.py
@@ -2,11 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.backend.api.routes import auth
 
-app = FastAPI()
+from src.backend.database.connection import init_db, get_connection
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn = get_connection()
+    try:
+        init_db(conn)
+    finally:
+        conn.close()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        # TODO: Add frontened origin
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
